59ca520abb22adf9fc22df8b148fffa43f09cf49
Ryan Nell <ryannell@hotmail.com>
2019-02-03 17:43:59 +0000

    Render Google Maps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { initMap, loadPlaces } from './utils'
 
 class App extends Component {
+
+  componentDidMount() {
+    let loadMap = initMap();
+    let placesPromise = loadPlaces();
+
+    // Take Array of promises to resolove
+    Promise.all([
+      loadMap,
+      placesPromise
+    ])
+    // Return Array of values
+    .then(values => {
+      let google = values[0];
+      let places = values[1].response.venues;
+
+      this.google = google;
+      this.markers = [];
+      console.log(values)
+
+      // Create new map
+      this.map = new google.maps.Map(document.getElementById('map') , {
+        center: {
+          lat: places[0].location.lat,
+          lng: places[0].location.lng
+        },
+        zoom: 12,
+      });
+
+      // Loop through each venue from foursquare and place a marker
+      places.forEach(place => {
+        let marker = new google.maps.Marker({
+          position: {
+            lat: place.location.lat,
+            lng: place.location.lng
+          },
+          map: this.map,
+          venue: place,
+          name: place.name,
+          animation: google.maps.Animation.DROP
+        });
+        this.markers.push(marker);
+        console.log(place)
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        
+      <div id="map">
+
       </div>
     );
   }
